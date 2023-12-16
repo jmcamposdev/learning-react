330fda71e65bae38aad51118412bcbb313dce57d
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-16 15:27:03 +0100

    Add Twitter Cards Project

---
+++ projects/01-twitter-follow-card/src/App.jsx
@@ -0,0 +1,37 @@
+import "./App.css";
+import { TwitterFolloCard } from "./TwitterFollowCard";
+
+export default function App() {
+  const users = [
+    {
+      userName: "jmcamposdev",
+      name: "José María Campos",
+      isFollowing: true,
+    },
+    {
+      userName: "hoxuro",
+      name: "Heriberto",
+      isFollowing: true,
+    },
+    {
+      userName: "hjimenezdev",
+      name: "Hugo Jiménez",
+      isFollowing: false,
+    },
+  ];
+  return (
+    <section className="card-container">
+      {users.map((user) => {
+        const { userName, name, isFollowing } = user;
+        return (
+          <TwitterFolloCard
+            key={userName}
+            userName={userName}
+            name={name}
+            initialIsFollowing={isFollowing}
+          />
+        );
+      })}
+    </section>
+  );
+}
---
+++ projects/01-twitter-follow-card/src/TwitterFollowCard.jsx
@@ -0,0 +1,35 @@
+import { useState } from "react";
+
+export function TwitterFolloCard({userName, name, initialIsFollowing}) {
+  const [isFollowing, setIsFollowing] = useState(initialIsFollowing);
+
+  const isFollowingText = isFollowing ? 'Siguiendo' : 'Seguir';
+  const buttonClassName = isFollowing ?
+  'tw-followCard-button is-following' :
+  'tw-followCard-button';
+
+  function handleIsFollowing () {
+    setIsFollowing(!isFollowing)
+  }
+  return (
+    <article className="tw-followCard">
+      <header className="tw-followCard-header">
+        <img
+          className="tw-followCard-avatar"
+          src={`https://unavatar.io/github/${userName}`}
+          alt={`El avatar de ${userName}`}
+        />
+        <div className="tw-followCard-info">
+          <strong>{name}</strong>
+          <span className="tw-followCard-infoUserName">@{userName}</span>
+        </div>
+      </header>
+
+      <aside>
+        <button className={buttonClassName} onClick={handleIsFollowing}>
+          {isFollowingText}
+        </button>
+      </aside>
+    </article>
+  );
+}
