aaf9bb21280bfcb43be00c297c2853fb78ff701c
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-17 11:10:23 +0100

    Refactor App component and add constants file

--- projects/02-tic-tac-toe/src/App.jsx
+++ projects/02-tic-tac-toe/src/App.jsx
@@ -1,15 +1,13 @@
 import { useState } from 'react';
 import './App.css'
 
-const TURNS = {
-  X: 'x',
-  O: 'o'
-}
+import { TURNS, WINNER_COMBINATIONS } from './constants';
+
+
 
 
 const Square = ({children, isSelected, updateBoard, index} ) => {
   const squareClassName = `square ${isSelected ? 'is-selected' : ''}`;
- 
   function handleClick() {
     updateBoard(index);
   }
@@ -20,22 +18,6 @@ const Square = ({children, isSelected, updateBoard, index} ) => {
     </div>
   )
 };
-
-const WINNER_COMBINATIONS = [
-  // Horizontal
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-
-  // Vertical
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-
-  // Diagonal
-  [0, 4, 8],
-  [2, 4, 6]
-];
 
 function App() {
   const [board, setBoard] = useState(Array(9).fill(null));
