cc17006af334868d61238bd41bdb9bd733db7f1c
José María <108521775+jmcamposdev@users.noreply.github.com>
2023-12-17 19:07:50 +0100

    Add random circles and hover functionality

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,15 +1,51 @@
 import { useEffect, useState } from 'react'
 
+/* ********************** */
+// Functions
+/* ********************** */
+const generateRandomPosition = () => {
+  const randomX = Math.floor(Math.random() * window.innerWidth)
+  const randomY = Math.floor(Math.random() * window.innerHeight)
+  return { x: randomX, y: randomY }
+}
+
+function isHovering (randomPosition, position) {
+  console.log('randomPosition', randomPosition.x)
+  console.log('position', position.x)
+  const differentX = randomPosition.x - position.x
+  const differentY = randomPosition.y - position.y
+  if (Math.abs(differentX) < 50 && Math.abs(differentY) < 50) {
+    console.log('isHovering')
+    return true
+  }
+  return false
+}
+
 function App () {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({ x: 0, y: 0 })
+  const [randomPosition, setRandomPosition] = useState(generateRandomPosition())
+  const [isCursorHoveringBall, setIsCursorHoveringBall] = useState(false)
+
+  /* ********************** */
+  // UseEffect
+  /* ********************** */
+  // Control the cursor circle
   useEffect(() => {
     const handleMove = (event) => {
       const { clientX, clientY } = event
       setPosition({ x: clientX, y: clientY })
+
+      if (isHovering(randomPosition, { x: clientX, y: clientY }) && enabled) {
+        setIsCursorHoveringBall(true)
+      }
     }
+
     if (enabled) {
+      // Add the event listener to the window
       window.addEventListener('pointermove', handleMove)
+      // Put the cursor ball in the cursor position
+      setPosition({ x: window.clientX, y: window.clientY })
     }
 
     // Clean the useEffect
@@ -20,28 +56,43 @@ function App () {
       // Reset the position
       setPosition({ x: 0, y: 0 })
     }
-  }, [enabled])
+  }, [enabled, randomPosition])
 
-  const handleCursorButton = () => {
-    setEnabled(!enabled)
-  }
+  // Control the random circles
+  useEffect(() => {
+    if (isCursorHoveringBall) {
+      setRandomPosition(generateRandomPosition())
+    }
+
+    // Clean the useEffect
+    return () => {
+      setIsCursorHoveringBall(false)
+    }
+  }, [isCursorHoveringBall])
   return (
     <main>
-      <div style={{
-        position: 'absolute',
-        backgroundColor: 'rgba(0, 0, 0, 0.5)',
-        border: '1px solid #fff',
-        borderRadius: '50%',
-        opacity: 0.8,
-        pointerEvents: 'none',
-        left: -25,
-        top: -25,
-        width: 50,
-        height: 50,
-        transform: `translate(${position.x}px, ${position.y}px)`
-      }}
+      <div
+        className='cursorCircle' style={{
+          position: 'absolute',
+          backgroundColor: 'rgba(0, 0, 0, 0.5)',
+          border: '1px solid #fff',
+          borderRadius: '50%',
+          opacity: 0.8,
+          pointerEvents: 'none',
+          left: -25,
+          top: -25,
+          width: 50,
+          height: 50,
+          transform: `translate(${position.x}px, ${position.y}px)`
+        }}
       />
-      <button onClick={handleCursorButton}>{enabled ? 'Disable' : 'Activate'} follow pointer</button>
+      <button onClick={() => { setEnabled(!enabled) }}>{enabled ? 'Disable' : 'Activate'} follow pointer</button>
+
+      <div
+        className='randomCircle' style={{
+          transform: `translate(${randomPosition.x}px, ${randomPosition.y}px)`
+        }}
+      />
     </main>
   )
 }
