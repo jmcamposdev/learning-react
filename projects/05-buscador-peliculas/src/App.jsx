ac15ad79fc84174751a108cf3f511ee53e03ee87
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-11 18:31:49 +0100

    Add search functionality to App component

--- projects/05-buscador-peliculas/src/App.jsx
+++ projects/05-buscador-peliculas/src/App.jsx
@@ -1,16 +1,27 @@
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
+import { useSearch } from './hooks/useSearch'
 
 function App () {
   const { movies } = useMovies()
+  const { search, updateSearch, error } = useSearch()
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+  }
+
+  const handleChange = (event) => {
+    updateSearch(event.target.value)
+  }
 
   return (
     <div className='page'>
       <h1>Buscador  Peliculas</h1>
       <header>
-        <form className='form'>
-          <input type='text' name='search' id='search' />
+        <form className='form' onSubmit={handleSubmit}>
+          <input type='text' name='query' id='query' onChange={handleChange} value={search} />
+          {error && <p style={{ color: 'red' }}>{error}</p>}
           <button type='submit'>Search</button>
         </form>
       </header>
