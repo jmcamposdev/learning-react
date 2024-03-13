ad943e3c5e3adcd28ef2420c9ee7225a8e50bd80
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-13 21:18:38 +0100

    Refactor App component to include sorting functionality

--- projects/05-buscador-peliculas/src/hooks/useMovies.js
+++ projects/05-buscador-peliculas/src/hooks/useMovies.js
@@ -1,19 +1,33 @@
-import { useRef, useState } from 'react'
+import { useCallback, useRef, useState } from 'react'
 import { searchMovies } from '../services/movies'
 
-export function useMovies ({ search }) {
+export function useMovies ({ search, sort }) {
   const [movies, setMovies] = useState([])
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
   const lastSearch = useRef(search)
 
   const getMovies = async () => {
-    if (lastSearch.current !== search) {
-      lastSearch.current = search
-    } else {
-      return
-    }
+    if (lastSearch.current === search) return
 
-    setMovies(await searchMovies(search))
+    try {
+      setLoading(true)
+      setError(null)
+      lastSearch.current = search
+      const newMovies = await searchMovies(search)
+      setMovies(newMovies)
+    } catch (error) {
+      setError('Error fetching movies')
+    } finally {
+      setLoading(false)
+    }
   }
 
-  return ({ movies, getMovies })
+  const sortMovie = useCallback(() => {
+    sort 
+      ? 
+
+  }, [sort, movies])
+
+  return ({ movies, getMovies, loading, error })
 }
