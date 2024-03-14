d0e590ccd37ece5cd17232f0dd798ab692d341ac
José María <108521775+jmcamposdev@users.noreply.github.com>
2024-03-14 22:46:16 +0100

    Refactor App component to include sorting functionality

--- projects/05-buscador-peliculas/src/App.jsx
+++ projects/05-buscador-peliculas/src/App.jsx
@@ -1,11 +1,13 @@
+import { useState } from 'react'
 import './App.css'
 import { Movies } from './components/Movies'
 import { useMovies } from './hooks/useMovies'
 import { useSearch } from './hooks/useSearch'
 
 function App () {
+  const [sort, setSort] = useState(false)
   const { search, updateSearch, error } = useSearch()
-  const { movies, getMovies } = useMovies({ search })
+  const { movies, getMovies, loading } = useMovies({ search, sort })
 
   const handleSubmit = (event) => {
     event.preventDefault()
@@ -16,19 +18,29 @@ function App () {
     updateSearch(event.target.value)
   }
 
+  const handleSort = () => {
+    setSort(!sort)
+  }
+
   return (
     <div className='page'>
       <h1>Buscador  Peliculas</h1>
       <header>
         <form className='form' onSubmit={handleSubmit}>
           <input type='text' name='query' id='query' onChange={handleChange} value={search} />
+          <label htmlFor='order'>
+            Order by name
+            <input type='checkbox' name='order' id='order' onChange={handleSort} checked={sort} />
+          </label>
           {error && <p style={{ color: 'red' }}>{error}</p>}
           <button type='submit'>Search</button>
         </form>
       </header>
 
       <main>
-        <Movies movies={movies} />
+        {loading
+          ? <p>Loading...</p>
+          : <Movies movies={movies} />}
       </main>
     </div>
   )
